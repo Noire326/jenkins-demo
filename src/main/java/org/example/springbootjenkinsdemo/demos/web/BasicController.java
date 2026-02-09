98e3a2b0d9e0295313224aa4240d3f8e2f001803
Noire326 <1429831694@qq.com>
2026-02-09 15:08:56 +0800

    test jenkins

--- src/main/java/org/example/springbootjenkinsdemo/demos/web/BasicController.java
+++ src/main/java/org/example/springbootjenkinsdemo/demos/web/BasicController.java
@@ -32,7 +32,7 @@ public class BasicController {
     @RequestMapping("/hello")
     @ResponseBody
     public String hello(@RequestParam(name = "name", defaultValue = "unknown user") String name) {
-        return "Hello[laoji oa,fang jia l ] " + name;
+        return "Hello[laoji oa,fang jia l sb id] " + name;
     }
 
     // http://127.0.0.1:8080/user
